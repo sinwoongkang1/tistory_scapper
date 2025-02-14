0279653eb21ca0d8a7bd20c9049de39662a8f1a4
kang <kkace22@gmail.com>
2025-02-14 14:44:33 +0900

    changed

--- folder/ChangeHandler.py
+++ folder/ChangeHandler.py
@@ -9,13 +9,27 @@ class ChangeHandler(FileSystemEventHandler):
         if event.is_directory:
             return
 
-        repo_path = '/home/kang/문서/bolg_scrap'  # Git 저장소 경로
+        # 무시할 파일 목록
+        ignored_files = [
+            'COMMIT_EDITMSG',
+            'main.lock',
+            'HEAD.lock',
+            'index.lock'
+        ]
+
+        # 파일 이름이 무시 목록에 포함되어 있는지 확인
+        if os.path.basename(event.src_path) in ignored_files:
+            return
+
+        repo_path = '/home/kang/문서/bolg_scrap/tistory_scapper'  # 올바른 Git 저장소 경로
         repo = git.Repo(repo_path)
 
+        # 변경 사항 추가 및 커밋
         repo.git.add(A=True)
         commit_message = f"Added: {os.path.basename(event.src_path)}"
         repo.index.commit(commit_message)
         print(f"Committed: {commit_message}")
+
 
 if __name__ == "__main__":
     path = '/home/kang/문서/bolg_scrap'  # 감지할 폴더
