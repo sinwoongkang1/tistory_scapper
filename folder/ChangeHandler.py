24b2d0368c56a1efb342cb900c8f8457c4b1688d
kang <kkace22@gmail.com>
2025-02-14 14:52:35 +0900

    changed

--- folder/ChangeHandler.py
+++ folder/ChangeHandler.py
@@ -5,8 +5,12 @@ from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 
 class ChangeHandler(FileSystemEventHandler):
+    def __init__(self):
+        super().__init__()
+        self.is_committing = False  # 커밋 중인지 여부를 나타내는 플래그
+
     def on_created(self, event):
-        if event.is_directory:
+        if event.is_directory or self.is_committing:
             return
 
         # 무시할 파일 목록
@@ -28,13 +32,19 @@ class ChangeHandler(FileSystemEventHandler):
         repo_path = '/home/kang/문서/bolg_scrap'  # 올바른 Git 저장소 경로
         repo = git.Repo(repo_path)
 
-        # 변경 사항 추가 및 커밋
-        repo.git.add(A=True)
-        commit_message = f"Added: {os.path.basename(event.src_path)}"
-        repo.index.commit(commit_message)
-        print(f"Committed: {commit_message}")
+        # 커밋 중 플래그 설정
+        self.is_committing = True
 
-
+        try:
+            # 변경 사항 추가 및 커밋
+            repo.git.add(A=True)
+            commit_message = f"Added: {os.path.basename(event.src_path)}"
+            repo.index.commit(commit_message)
+            print(f"Committed: {commit_message}")
+        finally:
+            # 커밋이 끝난 후 대기
+            time.sleep(10)  # 10초 대기
+            self.is_committing = False
 
 if __name__ == "__main__":
     path = '/home/kang/문서/bolg_scrap'  # 감지할 폴더
