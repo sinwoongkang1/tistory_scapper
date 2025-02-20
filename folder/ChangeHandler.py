828b41dd49ee1f3be4eab7b3badfbb52664c7bfb
sinwoongkang <kkace22@gmail.com>
2025-02-20 09:48:15 +0000

    Added: objkb440ogm

--- folder/ChangeHandler.py
+++ folder/ChangeHandler.py
@@ -1,4 +1,4 @@
-import os
+
 import git
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
@@ -35,39 +35,4 @@ class ChangeHandler(FileSystemEventHandler):
             return
 
         repo_path = '/home/ec2-user/blogScrapper'  # 올바른 Git 저장소 경로
-        repo = git.Repo(repo_path)
-
-        # 커밋 중 플래그 설정
-        self.is_committing = True
-
-        try:
-            # 변경 사항 추가 및 커밋
-            repo.git.add(A=True)
-            commit_message = f"Updated: {os.path.basename(event.src_path)}"
-            repo.index.commit(commit_message)
-            print(f"Committed: {commit_message}")
-
-            # 푸시 (upstream 설정 포함)
-            repo.git.push('origin', 'main')  # 원격 브랜치 설정 포함
-            print(f"Pushed: {commit_message}")
-
-        except Exception as e:
-            print(f"Error occurred: {e}")
-        finally:
-            self.is_committing = False  # 커밋이 끝난 후 플래그 해제
-
-if __name__ == "__main__":
-    path = '/home/ec2-user/blogScrapper'  # 감지할 폴더
-    event_handler = ChangeHandler()
-    observer = Observer()
-    observer.schedule(event_handler, path, recursive=True)
-
-    observer.start()
-    print(f"Watching for changes in {path}...")
-
-    try:
-        while True:
-            pass  # 무한 루프 대신 빈 루프 사용
-    except KeyboardInterrupt:
-        observer.stop()
-    observer.join()
+        repo = git.Repo(rep
